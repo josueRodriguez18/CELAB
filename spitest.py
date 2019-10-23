95f8b64166bf952a708a9710d45ed13f383f2936
Josue <josue.a.rodriguez@ttu.edu>
2019-10-22 22:06:00 -0700

    Deteled read, added drdy flag, added delay

--- spitest.py
+++ spitest.py
@@ -7,21 +7,35 @@ spi = spidev.SpiDev()
 
 gpio.setmode(gpio.BOARD)
 
-Gain = 64
-Vref = 5.0
-bitToVolt = 0
-
 rst = 11
 drdy = 13
-cs = 15
+sync = 15
 
-value = [120, 69, 150]
+df = 1
 
-def read():
-	spi.writebytes([0x01])
-	value = spi.readbytes(3)
-	value = conversion(value)
-	return value
+spi.open(0, 0)
+spi.mode = 1
+spi.max_speed_hz = 5000000 #5 MHz
+
+gpio.setup(rst, gpio.OUT) #RESET
+gpio.setup(drdy, gpio.IN) #DRDY
+gpio.setup(sync, gpio.OUT) #SYNC
+
+#value = [120, 69, 150]
+
+out = [None] * 32768
+
+spi.writebytes([0xFC]) #sync
+time.sleep(1)
+spi.writebytes([0x00]) #wakeup
+
+
+
+
+
+
+
+
 
 def conversion(value):
 	MSB = value[0]
@@ -30,20 +44,24 @@ def conversion(value):
 	new = MSB << 16 | MidB << 8 | LSB
 	return new
 
-gpio.setup(rst, gpio.OUT) #RESET
-gpio.setup(drdy, gpio.IN) #DRDY
-gpio.setup(cs, gpio.OUT) #CS
 
-spi.open(0, 0)
 
-out = [None] * 32768
 
-for i in range(0, 32768):
-	if (drdy):
-		out[i] = read()
-print(value)
-value = conversion(value)
-print(value)
+
+
+def main():
+	x = 0 #out index
+	df = 1 #previous data ready state flag
+	while(True):
+		if(drdy == 0 & df == 1): #if data ready just went from high to low
+			df = 0 #update drdy previous state
+			spi.writebytes([0x01]) #issue read command
+			time.sleep(.00001) #amount of time for 50 clock pulse delay (Rclk/Sclk x 50)
+			byteValue = spi.readbytes(3)  #take in data
+			out[x] = conversion(byteValue) #convert
+			x = x + 1 #increment index
+		
+main()
 
 
 
