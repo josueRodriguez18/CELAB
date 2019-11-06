5725d963a26b389c14eec834b6d08b2e290101fa
Josue <josue.a.rodriguez@ttu.edu>
2019-11-05 23:21:59 -0700

    spi stuff

--- spitest.py
+++ spitest.py
@@ -1,4 +1,5 @@
 import time
+import timeit
 import spidev
 import RPi.GPIO as gpio
 import random
@@ -17,13 +18,15 @@ spi.open(0, 0)
 spi.mode = 1
 spi.max_speed_hz = 5000000 #5 MHz
 
+gpio.setwarnings(False)
+
 gpio.setup(rst, gpio.OUT) #RESET
 gpio.setup(drdy, gpio.IN) #DRDY
 gpio.setup(sync, gpio.OUT) #SYNC
 
 #value = [120, 69, 150]
 
-out = [None] * 32768
+out = [1] * 32768
 
 spi.writebytes([0xFC]) #sync
 time.sleep(1)
@@ -41,7 +44,7 @@ def conversion(value):
 	MSB = value[0]
 	MidB = value[1]
 	LSB = value[2]
-	new = MSB << 16 | MidB << 8 | LSB
+	new = (MSB << 16) | (MidB << 8) | (LSB)
 	return new
 
 
@@ -51,42 +54,75 @@ def conversion(value):
 
 def main():
 	x = 0 #out index
+	t = 1
+	drc = 5
 	df = 1 #previous data ready state flag
 	while(True):
-		if(gpio.input(drdy) == 0 and df == 1): #if data ready just went from high to low
+		if (x == 32768): #32768
+			for j in range(1, 32768): #32768
+				print(out[j])
+				print(j)
+			break
+		if (gpio.input(drdy) == 0 and df == 1): #if data ready just went from high to low
+			if(drc == 0):
+				spi.writebytes([0x01])
+				for y in range(1, 9115): #wait for 6.51 uS = 9115
+					pass
+				byteValue = spi.readbytes(3)  #take in data
+				out[x] = byteValue  #convert
+				#print(conversion(byteValue)/(2**24))
+				x = x + 1 #increment index
+				#print(x)
 			df = 0 #update drdy previous state
-			spi.writebytes([0x01]) #issue read command
-#			time.sleep(.00000651) #amount of time for 50 clock pulse delay (Rclk/Sclk x 50)
-			byteValue = spi.readbytes(3)  #take in data
-			out[x] = conversion(byteValue) #convert
-			print(byteValue)
-			x = x + 1 #increment index
-		if (gpio.input(drdy) == 1):
-			df = 1
+		if (gpio.input(drdy) == 1 and df == 0):
+			drc = drc - 1
+			if (drc == 0):
+				df = 1
+				drc = 5
+
 
 
 		#spi.writebytes([0x01]) <-- uncomment this  if you wanna probe the clock output
 
-#main()
-
-testint = 0
-
-while(True):
-	spi.writebytes([0x01])
-	time.sleep(.000002)
-	testint = spi.readbytes(3)
-	#print(testint)
-df = 1
-#while(True):
-#	if(gpio.input(drdy) == 0 and df == 1): #if data ready just went from high to low
-#	if(gpio.input(drdy) == 0): #non df based if statement
-#		df = 0 #update drdy previous state
-#		spi.writebytes([0x01]) #issue read command
-		#time.sleep(.0001) #amount of time for 50 clock pulse delay (Rclk/Sclk x 50)
-	#	byteValue = spi.readbytes(3)  #take in data
-#	if (gpio.input(drdy) == 1):
-#		df = 1
+main()
 
 
+
+
+
+
+
+
+
+def tester():
+#	testint = 0
+#	while(True):
+#		spi.writebytes([0x01])
+#		start = time.time()
+#		for x in range(1, 9115):
+#			pass
+#
+#		#print(elapsed)
+#		#counter for delay
+#		testint = spi.readbytes(3)
+#		print(testint)
+
+
+	s ='''x = 0
+	df = 1 #previous data ready state flag
+	while(True):
+		if (x == 32768):
+			break
+		if (gpio.input(drdy) == 0 and df == 1): #if data ready just went from high to low
+			spi.writebytes([0x01]) #issue read command
+			byteValue = spi.readbytes(3)  #take in data
+			out[x] = byteValue  #convert
+			x = x + 1 #increment index
+			df = 0 #update drdy previous state
+		if (gpio.input(drdy) == 1):
+			df = 1'''
+	print timeit.timeit(stmt = s, number = 100)
+
+#tester()
 
 
