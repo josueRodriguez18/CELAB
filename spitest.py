e25707f6d77bf7dddeb8ae26879c449b7c3a9b3f
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2019-11-12 18:56:44 -0600

    added wiringPi program priority to reduce overhead

--- spitest.py
+++ spitest.py
@@ -37,10 +37,7 @@ def conversion(value):
 	return new
 
 def oneShot():
-	#for j in range(0, 46200): #delay between oneShots, not accurate (should be 33 microseconds)
-	#	pass #do nothing
-	#df = 1 #set data flag to one
-	wiringpi.delayMicroseconds(33)
+	wiringpi.delayMicroseconds(33) #delay between conversions(30ksps)
 	spi.writebytes([0x00]) #WAKEUP command
 	while(not gpio.input(drdy) == 0) : #wait until drdy goes low
 		pass
