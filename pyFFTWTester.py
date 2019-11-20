6eea54665e45f3b070841a944b30d0751fcdf285
Josue <josue.a.rodriguez@ttu.edu>
2019-11-19 22:43:26 -0700

    Fixed conversions on spitest, using 555 timer clock for delays, changed some bits on FFTWTester, Removed Herobrine

--- pyFFTWTester.py
+++ pyFFTWTester.py
@@ -1,14 +1,19 @@
+import math
 import pyfftw
 import numpy
 import spitest
-import spitest.NUM_ELEMENTS as NUM_ELEMENTS
+from spitest import NUM_ELEMENTS
+from spitest import out as IN
 Fs = 30000
 
+absOut = [None] * NUM_ELEMENTS
 def fastFourier():
-    input = pyfftw.empty_aligned(NUM_ELEMENTS, dtype ='complex128')
-    output = pyfftw.empty_aligned(NUM_ELEMENTS, dtype ='complex128')
-    fft_object = pyfftw.FFTW(input, output)
-    return output
+	input = pyfftw.empty_aligned(NUM_ELEMENTS, dtype ='complex128')
+	for i in range(0, NUM_ELEMENTS):
+		input[i] = IN[i]
+	output = pyfftw.empty_aligned(NUM_ELEMENTS, dtype ='complex128')
+	fft_object = pyfftw.FFTW(input, output)
+	return output
 
 def max( arr ):
 	max = 0
@@ -20,8 +25,10 @@ def max( arr ):
 	return k
 
 def fourierABS( arr ):
-    for x in arr:
-        absOut[x] = sqrt(arr[x].real^2 + arr[x].imag^2)
-    return absOut
+	for x in range(0, NUM_ELEMENTS):
+		absOut[x] = math.sqrt(arr[x].real**2 + arr[x].imag**2)
+	return absOut
 
 freq = max(fastFourier())*Fs/NUM_ELEMENTS
+spitest.main()
+print(max(fourierABS(fastFourier())))
--- spitest.py
+++ spitest.py
@@ -12,8 +12,9 @@ gpio.setmode(gpio.BOARD)
 rst = 11
 drdy = 13
 sync = 15
+timer = 16
 
-df = 1
+NUM_ELEMENTS = 4096
 
 spi.open(0, 0)
 spi.mode = 1
@@ -24,25 +25,28 @@ gpio.setwarnings(False)
 gpio.setup(rst, gpio.OUT) #RESET
 gpio.setup(drdy, gpio.IN) #DRDY
 gpio.setup(sync, gpio.OUT) #SYNC
+gpio.setup(timer, gpio.IN) #555 Timer
 
-out = [None] * 32768  #32768 original size (shortened to 1024 for testing purposes)
+out = [None] * NUM_ELEMENTS  #32768 original size (shortened to 1024 for testing purposes)
 
 spi.writebytes([0xFC]) #sync
-wiringpi.delayMicroseconds(1000000)
 spi.writebytes([0x00]) #wakeup
 
 def conversion(value):
+	#this number is in 2's comp format
 	MSB = value[0]
 	MidB = value[1]
 	LSB = value[2]
 	new = (MSB << 16) | (MidB << 8) | (LSB)
+	sign = MSB & 0b10000000 #check sign bit to see if complement is necessary
+	sign = sign >> 7
+	if sign: #2's comp
+		new = ~new
+		new = new + 0b1
 	return new
 
 def oneShot():
-	#for j in range(0, 46200): #delay between oneShots, not accurate (should be 33 microseconds)
-	#	pass #do nothing
-	#df = 1 #set data flag to one
-	wiringpi.delayMicroseconds(33)
+	count(3) #count x 33 us periods
 	spi.writebytes([0x00]) #WAKEUP command
 	while((gpio.input(drdy)) == 1) : #wait until drdy goes low
 		pass
@@ -53,24 +57,34 @@ def oneShot():
 	spi.writebytes([0xFD]) #put back in standby mode
 	return byteValue #return for conversion
 
+def count(y):
+	df = 0 #data flag to count rises
+	x = 0
+	while(x < y): # num*3 equals 33 us periods
+		if(gpio.input(timer) == 1 and df == 0): #rise
+			x = x + 1 #increment counter
+			df = 1 #reset df
+		if (gpio.input(timer) == 0 and df == 1): #fall
+			df = 0 #reset df
 
-spi.writebytes([0xFD]) #begin standby mode
-x = 0 #array index
-start = time.clock() #start time for sample
-while x < 32768: #1024 samples
-	out[x] = conversion(oneShot()) #take sample
-	x = x+1 #increment index
-elapsed = time.clock() - start #total elapsed time
-for y in range(0, 32768):
-	print(out[y]) #print out values for testing
-print(elapsed) #print elapsed time (elapsed time/1024 = deltaX = time between array indeces
-#k = 0
-#for y in range(2, 32768): #look for similar values and return the differences between their indexes
-#	if out[y] <= out[1] + 5 and out[y] >= out[1] - 5:
-#		k = y - 1
-#print(k)
-xaxis = [0]*32768
-for i in range(1,32768):
-	xaxis[i] = i
-plt.plot(xaxis, out)
-plt.show()
+def main():
+	spi.writebytes([0xFD]) #begin standby mode
+	x = 0 #array index
+	start = time.clock() #start time for sample
+	while x < NUM_ELEMENTS: #1024 samples
+		out[x] = conversion(oneShot()) #take sample
+		x = x+1 #increment index
+	elapsed = time.clock() - start #total elapsed time
+	avgel = elapsed/NUM_ELEMENTS
+	for y in range(0, NUM_ELEMENTS):
+		print(out[y]) #print out values for testing
+	print(elapsed) #print elapsed time (elapsed time/1024 = deltaX = time between array indeces
+	print(avgel)
+	xaxis = [0]*NUM_ELEMENTS
+	for i in range(1,NUM_ELEMENTS):
+		xaxis[i] = i
+	plt.plot(xaxis, out)
+	plt.show()
+	#arayout = matplotlib.pyplot.specgram(out, 1024, 30000, 0)
+	#plt.plot()
+main()
