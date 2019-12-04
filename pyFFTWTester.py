b7e0c96ac48d837a2723611b35abdb4f0611d200
Josue <josue.a.rodriguez@ttu.edu>
2019-12-04 11:26:00 -0700

    some lookup table changes

--- pyFFTWTester.py
+++ pyFFTWTester.py
@@ -10,6 +10,8 @@ Fs = 7522
 NOTERY = { "C":16.35, "C#":17.32, "D":18.35, "D#":19.45, "E#":20.60,"F":21.83, "F#":23.12, "G":24.5, "G#":25.96, "A":27.5, "A#":29.14, "B":30.87, 
  16.35:"C", 17.32:"C#", 18.35:"D", 19.45:"D#", 20.60:"E#", 21.83:"F", 23.12:"F#", 24.5:"G", 25.96:"G#", 27.5:"A", 29.14:"A#", 30.87:"B"}
 
+octave = 0
+
 # absOut = [None] * NUM_ELEMENTS
 # def fastFourier():
 # 	input = pyfftw.empty_aligned(NUM_ELEMENTS, dtype ='complex128')
@@ -38,7 +40,7 @@ def max( arr ):
 
 def nFourier():
 	x = np.array(IN)
-	output = fft(x)
+	output = np.fft(x)
 	output = nFourier()
 	output[0] = 0
 	output = np.abs(output)
@@ -46,35 +48,32 @@ def nFourier():
 
 
 def noteTable(note):
-	octave = 0		
-	if ( 11.00 < note and note < 17.32): 
+	while ( note > 32.7 ):
+		note = note/2
+		octave = octave + 1
+	if ( note <= 17.32):
 		result = "C"
-	elif( 17.32 < note and note < 18.35): 
+	elif( 17.32 < note and note <= 18.35):
 		result = "C#"
-	elif( 18.35 < note and note < 19.45):
+	elif( 18.35 < note and note <= 19.45):
 		result = "D"
-	elif( 19.45 < note and note < 20.60):
+	elif( 19.45 < note and note <= 20.60):
 		result = "D#"
-	elif( 20.60 < note and note < 21.83):
+	elif( 20.60 < note and note <= 21.83):
 		result = "E"
-	elif( 21.83 < note and note < 23.12):
+	elif( 21.83 < note and note <= 23.12):
 		result = "F"
-	elif( 23.12 < note and note < 24.50):
+	elif( 23.12 < note and note <= 24.50):
 		result = "F#"
-	elif( 24.50 < note and note < 25.96):
+	elif( 24.50 < note and note <= 25.96):
 		result = "G"
-	elif( 25.96 < note and note < 27.50):
+	elif( 25.96 < note and note <= 27.50):
 		result = "A"
-	elif( 29.14 < note and note < 30.87):
+	elif( 29.14 < note and note <= 30.87):
 		result = "A#"
-	elif( 30.87 < note and note < 32.70):
+	elif( 30.87 < note and note <= 32.70):
 		result = "B"
-	elif( 32.70 < note and note < 33.0):
-		while (note > 32.7):
-			note = note/2
-			octave += 1
-		result = noteTable(note)
-	return result, octave
+	return result
 
 
 def inOut( freq ):
